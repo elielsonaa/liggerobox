c554892cb213cd8773dd9a148094d3868be74e19
liggerobox <elielson@gd.com.br>
2021-02-05 20:48:42 -0300

    Antes do DrawerNavigator

--- App.js
+++ App.js
@@ -1,70 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {
-  SafeAreaView,
+  View,
   StyleSheet,Text
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
-
 const App = () =>  {
   return (
-    <>
-      <SafeAreaView>
-        <Text>teste</Text>
-      </SafeAreaView>
-    </>
+    <View style={styles.screen}>
+      <Text>Teste</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  screen:{ 
+     flex:1,
+     alignItems:"center",
+     justifyContent: "center",
+     backgroundColor:"#FBB000"
+  }
 });
 
 export default App;
